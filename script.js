30b65db73022a95c6f2b84bcd4937498418a6bf7
Justine Patoux <justine.patoux@gmail.com>
2023-11-24 18:50:36 +0100

    feat: fill canvas cells when hovering

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const grid = document.getElementById('canvas');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const cell = document.createElement('div');
-        cell.classList.add('cell');
+        cell.classList.add('canvas__cell');
         grid.appendChild(cell);
     }
 }
+
+const pixels = document.querySelectorAll('.canvas__cell');
+
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('canvas__cell--filled');
+    })
+});
